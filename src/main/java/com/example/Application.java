95af12c7d26b6fc4e7c4ba9abe0e5c751700bffa
KangYong <arahansa@naver.com>
2015-03-15 19:21:40 +0900

    first commit

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by arahansa on 2015-03-15.
+ */
+@SpringBootApplication
+@ComponentScan("controller")
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/controller/Testcontroller.java
@@ -0,0 +1,25 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by arahansa on 2015-03-15.
+ */
+@Controller
+public class Testcontroller {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String helloworld(){
+        return "hello world";
+    }
+
+    @RequestMapping(value = "/thymeleaf")
+    String thymeleaf(Model model){
+        model.addAttribute("message", "hello thymeleaf");
+        return "index";
+    }
+}
